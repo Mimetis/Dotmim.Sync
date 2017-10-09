3ca25fe97189546ee0586d5fd9a141a142ce380f
spertus <spertus@microsoft.com>
2017-10-09 22:05:47 +0200

    Adding FX 4.6 Tests for MySql

---
+++ Tests/Dotmim.Sync.FX.Tests/SqlUtils/InlineConfigurations.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dotmim.Sync.FX.Tests.SqlUtils
+{
+    public class InlineConfigurations : IEnumerable<object[]>
+    {
+        private readonly List<object[]> confs = new List<object[]>();
+
+        public InlineConfigurations()
+        {
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 100,
+                UseBulkOperations = true,
+                SerializationFormat= Enumerations.SerializationFormat.Json
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 100,
+                UseBulkOperations = false,
+                SerializationFormat= Enumerations.SerializationFormat.Json
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 0,
+                UseBulkOperations = true,
+                SerializationFormat= Enumerations.SerializationFormat.Json
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 0,
+                UseBulkOperations = false,
+                SerializationFormat= Enumerations.SerializationFormat.Json
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 100,
+                UseBulkOperations = true,
+                SerializationFormat= Enumerations.SerializationFormat.DmSerializer
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 100,
+                UseBulkOperations = false,
+                SerializationFormat= Enumerations.SerializationFormat.DmSerializer
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 0,
+                UseBulkOperations = true,
+                SerializationFormat= Enumerations.SerializationFormat.DmSerializer
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 0,
+                UseBulkOperations = false,
+                SerializationFormat= Enumerations.SerializationFormat.DmSerializer
+            } });
+
+        }
+        public IEnumerator<object[]> GetEnumerator() => confs.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    }
+}
