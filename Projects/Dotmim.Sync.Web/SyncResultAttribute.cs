464d6d45e950689ea0152ffd195cd7beca09dda2
PGrasseels <grasseelsp@hotmail.com>
2018-05-24 19:26:28 +0200

    Create Filter Attribute for AutoSync on controller

---
+++ Projects/Dotmim.Sync.Web/SyncResultAttribute.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dotmim.Sync.Web
+{
+    public class SyncResultAttribute : ResultFilterAttribute, IResultFilter, IAsyncResultFilter
+    {
+        public string[] OnMethods { get; set; } = new[] { "POST" };
+
+        public override async void OnResultExecuted(ResultExecutedContext context)
+        {
+            base.OnResultExecuted(context);
+
+            await SyncContextAsync(context);
+        }
+
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            await SyncContextAsync(context);
+
+            await next();
+        }
+
+        private async Task SyncContextAsync(FilterContext context)
+        {
+            if (OnMethods.Contains(context.HttpContext.Request.Method))
+            {
+                WebProxyServerProvider _webProxyService = (WebProxyServerProvider)context.HttpContext.RequestServices.GetService(typeof(WebProxyServerProvider));
+
+                if (_webProxyService == null) { 
+                    throw new ArgumentNullException("Proxy service not found");
+                }
+
+                await _webProxyService.HandleRequestAsync(context.HttpContext);
+            }
+        }
+    }
+}
