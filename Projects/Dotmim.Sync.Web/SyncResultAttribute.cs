a187be3b8d107eede3147ba3623caa93a29926e0
PGrasseels <grasseelsp@hotmail.com>
2018-05-24 19:42:38 +0200

    Add constructor and usage

--- Projects/Dotmim.Sync.Web/SyncResultAttribute.cs
+++ Projects/Dotmim.Sync.Web/SyncResultAttribute.cs
@@ -7,7 +7,25 @@ namespace Dotmim.Sync.Web
 {
     public class SyncResultAttribute : ResultFilterAttribute, IResultFilter, IAsyncResultFilter
     {
+        /// <summary>
+        /// Methods trig synchronisation
+        /// </summary>
         public string[] OnMethods { get; set; } = new[] { "*" };
+
+        public SyncResultAttribute()
+        {
+
+        }
+
+        /// <summary>
+        /// Initialise methods trig sync, separe by comma
+        /// Sample : POST,GET,DELETE
+        /// </summary>
+        /// <param name="methods">String represent the methods trig synchronisation</param>
+        public SyncResultAttribute(string methods)
+        {
+            OnMethods = methods.Replace(" ", "").Split(',');
+        }
 
         public override async void OnResultExecuted(ResultExecutedContext context)
         {
@@ -25,11 +43,13 @@ namespace Dotmim.Sync.Web
 
         private async Task SyncContextAsync(FilterContext context)
         {
-            if (OnMethods.Contains(context.HttpContext.Request.Method) || OnMethods.Contains("*"))
+            if (OnMethods.Contains(context.HttpContext.Request.Method)
+                || OnMethods.Contains("*"))
             {
                 WebProxyServerProvider _webProxyService = (WebProxyServerProvider)context.HttpContext.RequestServices.GetService(typeof(WebProxyServerProvider));
 
-                if (_webProxyService == null) { 
+                if (_webProxyService == null)
+                {
                     throw new ArgumentNullException("Proxy service not found");
                 }
 
