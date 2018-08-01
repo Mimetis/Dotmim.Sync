c3acfe72e0163216a0cab2bbe57303a7281b3620
alexander.marek <alexander.marek@opti-q.com>
2018-08-01 17:09:13 +0200

    Added net462 target

--- Projects/Dotmim.Sync.Web/SyncResultAttribute.cs
+++ Projects/Dotmim.Sync.Web/SyncResultAttribute.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿#if NETSTANDARD
+using Microsoft.AspNetCore.Mvc.Filters;
+#else
+using System.Web.Http.Filters;
+using System.Web;
+#endif
 using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Http.Controllers;
 
 namespace Dotmim.Sync.Web
 {
+#if NETSTANDARD
     public class SyncResultAttribute : ResultFilterAttribute, IResultFilter, IAsyncResultFilter
     {
         /// <summary>
@@ -57,4 +65,62 @@ namespace Dotmim.Sync.Web
             }
         }
     }
+#else
+    public class SyncResultAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Methods trig synchronisation
+        /// </summary>
+        public string[] OnMethods { get; set; } = new[] { "*" };
+
+        public SyncResultAttribute()
+        {
+
+        }
+
+        /// <summary>
+        /// Initialise methods trig sync, separe by comma
+        /// Sample : POST,GET,DELETE
+        /// </summary>
+        /// <param name="methods">String represent the methods trig synchronisation</param>
+        public SyncResultAttribute(string methods)
+        {
+            OnMethods = methods.Replace(" ", "").Split(',');
+        }
+
+        public override async Task OnActionExecutedAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            bool wasSuccessful = actionExecutedContext.Exception == null;
+            if (!wasSuccessful)
+                return;
+
+            await SyncContextAsync(actionExecutedContext.ActionContext);
+
+            await base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
+        }
+
+        public override async Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            await SyncContextAsync(actionContext);
+
+            await base.OnActionExecutingAsync(actionContext, cancellationToken);
+        }
+
+        private async Task SyncContextAsync(HttpActionContext context)
+        {
+            if (OnMethods.Contains(context.Request.Method.Method)
+                || OnMethods.Contains("*"))
+            {
+                WebProxyServerProvider _webProxyService = (WebProxyServerProvider)context.HttpContext.RequestServices.GetService(typeof(WebProxyServerProvider));
+
+                if (_webProxyService == null)
+                {
+                    throw new ArgumentNullException("Proxy service not found");
+                }
+
+                await _webProxyService.HandleRequestAsync(context.HttpContext);
+            }
+        }
+    }
+#endif
 }
