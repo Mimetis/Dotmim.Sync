05e0d9c9c57ad85937d2a8cba42ce7ea7c73a817
alexander.marek <alexander.marek@opti-q.com>
2018-08-21 15:56:14 +0200

    Added first WebApi2 integration test

---
+++ Tests/Dotmim.Sync.WebApi2.Tests/SqlUtils/InlineConfigurations.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dotmim.Sync.Test.SqlUtils
+{
+    public class InlineConfigurations : IEnumerable<object[]>
+    {
+        private readonly List<object[]> confs = new List<object[]>();
+
+        public InlineConfigurations()
+        {
+            confs.Add(new object[] { new SyncConfiguration{
+                
+                DownloadBatchSizeInKB = 100,
+                UseBulkOperations = true,
+                SerializationFormat= Enumerations.SerializationFormat.Json
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 100,
+                UseBulkOperations = false,
+                SerializationFormat= Enumerations.SerializationFormat.Json
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 0,
+                UseBulkOperations = true,
+                SerializationFormat= Enumerations.SerializationFormat.Json
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 0,
+                UseBulkOperations = false,
+                SerializationFormat= Enumerations.SerializationFormat.Json
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 100,
+                UseBulkOperations = true,
+                SerializationFormat= Enumerations.SerializationFormat.Binary
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 100,
+                UseBulkOperations = false,
+                SerializationFormat= Enumerations.SerializationFormat.Binary
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 0,
+                UseBulkOperations = true,
+                SerializationFormat= Enumerations.SerializationFormat.Binary
+            } });
+
+            confs.Add(new object[] { new SyncConfiguration{
+                DownloadBatchSizeInKB = 0,
+                UseBulkOperations = false,
+                SerializationFormat= Enumerations.SerializationFormat.Binary
+            } });
+
+        }
+        public IEnumerator<object[]> GetEnumerator() => confs.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    }
+}
