d460fc7d21dd465102f37aad7befd00f54561fcf
alexander.marek <alexander.marek@opti-q.com>
2018-08-24 10:10:08 +0200

    Switched from SqlDbType to DbType

--- Projects/Dotmim.Sync.Core/Filter/FilterClause.cs
+++ Projects/Dotmim.Sync.Core/Filter/FilterClause.cs
@@ -13,14 +13,25 @@ namespace Dotmim.Sync.Filter
 
         public String ColumnName { get; set; }
 
-        public SqlDbType? Type { get; set; }
+        public DbType? ColumnType { get; set; }
 
-        public bool IsVirtual => Type.HasValue;
+        /// <summary>
+        /// Gets whether the filter is targeting an existing column of the target table (not virtual) or it is only used as a parameter in the selectchanges stored procedure (virtual)
+        /// </summary>
+        public bool IsVirtual => ColumnType.HasValue;
         
-        public FilterClause(string tableName, string columnName, SqlDbType? type)
+        /// <summary>
+        /// Creates a filterclause allowing to specify a DbType.
+        /// If you specify the columnType, Dotmim.Sync will expect that the column does not exist on the table, and the filter is only
+        /// used as a parameter for the selectchanges stored procedure. Thus, IsVirtual would be true
+        /// </summary>
+        /// <param name="tableName">The table to be filtered</param>
+        /// <param name="columnName">The name of the column - or the filterparameter</param>
+        /// <param name="columnType">Pass null to filter on a real table column, pass a DbType in case the filter should only be a parameter in the selectchanges stored procedure</param>
+        public FilterClause(string tableName, string columnName, DbType? columnType)
             : this(tableName, columnName)
         {
-            Type = type;
+            ColumnType = columnType;
         }
 
         public FilterClause(string tableName, string columnName)
