475f92c535d8b1c33538336d8c91f03eb3ed9032
Saverio Cisternino <workgroupengineering@outlook.com>
2018-09-13 15:08:31 +0200

    Added models

---
+++ Tests/Dotmim.Sync.Tests/Models/PriceListDetail.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Dotmim.Sync.Tests.Models
+{
+    public class PriceListDetail
+    {
+        public PriceListCategory Category { get; set; }
+
+        public Guid PriceListId { get; set; }
+        public string PriceCategoryId { get; set; }
+        public Guid PriceListDettailId { get; set; }
+
+        public Guid ProductId { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Discount { get; set; }
+        public int? MinQuantity { get; set; }
+    }
+}
