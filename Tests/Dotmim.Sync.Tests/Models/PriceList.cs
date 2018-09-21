8db2f48a14eff307737a9eec71fe786254dd2eb1
Sébastien Pertus <spertus@microsoft.com>
2018-09-21 16:32:34 +0200

    Make PriceList.From nullable to avoid DateTime.Minvalue as initial value

--- Tests/Dotmim.Sync.Tests/Models/PriceList.cs
+++ Tests/Dotmim.Sync.Tests/Models/PriceList.cs
@@ -12,7 +12,7 @@ namespace Dotmim.Sync.Tests.Models
 
         public Guid PriceListId { get; set; }
         public string Description { get; set; }
-        public DateTime From { get; set; }
+        public DateTime? From { get; set; }
         public DateTime? To { get; set; }
         public IList<PriceListCategory> Categories { get; set; }
     }
