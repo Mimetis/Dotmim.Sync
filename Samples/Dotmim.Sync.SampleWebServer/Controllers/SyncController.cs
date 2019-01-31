657114d7f865f8381b764b1ccc9911c764e26880
Sébastien Pertus <spertus@microsoft.com>
2019-01-31 15:11:20 +0100

    in progress

---
+++ Samples/Dotmim.Sync.SampleWebServer/Controllers/SyncController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dotmim.Sync.Web.Server;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dotmim.Sync.SampleWebServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SyncController : ControllerBase
+    {
+        private WebProxyServerProvider webProxyServer;
+
+        // Injected thanks to Dependency Injection
+        public SyncController(WebProxyServerProvider proxy)
+        {
+            webProxyServer = proxy;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task Post()
+        {
+            await webProxyServer.HandleRequestAsync(this.HttpContext);
+        }
+    }
+}
