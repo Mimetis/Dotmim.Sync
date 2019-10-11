1cfcce80d516d23d682c2c47244a27f8dc73d21a
Sébastien Pertus <spertus@microsoft.com>
2019-10-11 13:06:17 +0200

    First try with Orchestrator

---
+++ Projects/Dotmim.Sync.Core/LocalOrchestrator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotmim.Sync
+{
+    public class LocalOrchestrator : ILocalOrchestrator<IProvider>
+    {
+        public Task<string> ApplyChangesAsync(object remoteChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> GetChangesAsync(object localChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void On(InterceptorBase interceptor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void On<T>(Action<T> interceptorAction) where T : ProgressArgs
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetConfiguration(Action<SyncConfiguration> configuration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetOptions(Action<SyncOptions> options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetProgress(IProgress<ProgressArgs> progress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetProvider(IProvider coreProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
