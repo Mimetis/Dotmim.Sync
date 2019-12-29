6d732d1420391a68d6cdf88e7e09812a5cfd2b1b
Sébastien Pertus <spertus@microsoft.com>
2019-12-29 14:58:30 +0100

    Add Tests, Handle conflict Delete-Delete correctly

--- Projects/Dotmim.Sync.Core/Enumerations/ConflictType.cs
+++ Projects/Dotmim.Sync.Core/Enumerations/ConflictType.cs
@@ -24,28 +24,22 @@ namespace Dotmim.Sync.Enumerations
         /// <summary>
         /// The Remote and Local datasources both updated the same row.
         /// </summary>
-        RemoteUpdateLocalUpdate,
+        RemoteExistsLocalExists,
 
         /// <summary>
         /// The Remote datasource updated a row that the Local datasource deleted.
         /// </summary>
-        RemoteUpdateLocalDelete,
-
-        /// <summary>
-        /// The Remote datasource deleted a row that the Local datasource insert with the same key.
-        /// </summary>
-        RemoteDeleteLocalInsert,
+        RemoteExistsLocalIsDeleted,
 
         /// <summary>
         /// The Remote datasource deleted a row that the Local datasource updated.
         /// </summary>
-        RemoteDeleteLocalUpdate,
+        RemoteIsDeletedLocalExists,
 
         /// <summary>
         /// The Remote and Local datasource both deleted the same row.
         /// </summary>
-        RemoteDeleteLocalDelete,
-
+        RemoteIsDeletedLocalIsDeleted,
  
         /// <summary>
         /// The Remote peer deleted a row that the Local peer updated, and the metadata for that row was cleaned up.
@@ -55,11 +49,11 @@ namespace Dotmim.Sync.Enumerations
         /// <summary>
         /// The Remote peer update a row that the Local never had.
         /// </summary>
-        RemoteUpdateLocalNoRow,
+        RemoteExistsLocalNotExists,
 
         /// <summary>
         /// The Remote peer delete a row that the Local never had.
         /// </summary>
-        RemoteDeleteLocalNoRow
+        RemoteIsDeletedLocalNotExists
     }
 }
