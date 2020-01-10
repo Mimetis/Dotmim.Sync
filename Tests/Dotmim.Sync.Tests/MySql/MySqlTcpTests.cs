443595029f685f32e3499114c9b380e3dc383801
Sébastien Pertus <spertus@microsoft.com>
2020-01-10 09:27:11 +0100

    Test again now that pool is fixed !

--- Tests/Dotmim.Sync.Tests/MySql/MySqlTcpTests.cs
+++ Tests/Dotmim.Sync.Tests/MySql/MySqlTcpTests.cs
@@ -28,7 +28,7 @@ namespace Dotmim.Sync.Tests
         };
 
         public override List<ProviderType> ClientsType => new List<ProviderType>
-            { ProviderType.MySql}; 
+            { ProviderType.MySql, ProviderType.Sqlite, ProviderType.Sql}; 
 
         public override ProviderType ServerType =>
             ProviderType.MySql;
