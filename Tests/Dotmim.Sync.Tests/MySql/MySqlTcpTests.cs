ab3c09010717c432a3062edfe89f12baafb33c25
Sébastien Pertus <spertus@microsoft.com>
2020-01-17 21:27:57 +0100

    configure tests

--- Tests/Dotmim.Sync.Tests/MySql/MySqlTcpTests.cs
+++ Tests/Dotmim.Sync.Tests/MySql/MySqlTcpTests.cs
@@ -28,7 +28,7 @@ namespace Dotmim.Sync.Tests
         };
 
         public override List<ProviderType> ClientsType => new List<ProviderType>
-            { ProviderType.MySql, ProviderType.MySql}; 
+            { ProviderType.MySql, ProviderType.Sql, ProviderType.Sqlite};
 
         public override ProviderType ServerType =>
             ProviderType.MySql;
