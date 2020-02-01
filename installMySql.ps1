bc69b56dab96391a9667799c67a02ce5d25560ac
Sébastien Pertus <spertus@microsoft.com>
2020-02-01 15:59:58 +0100

    clean files

--- installMySql.ps1
+++
@@ -1,8 +0,0 @@
-# install chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-#install mysql
-cinst mysql -y
-
-#set initial root user password to be the one used by dotmim sync
-mysqladmin -u root password Password12!
