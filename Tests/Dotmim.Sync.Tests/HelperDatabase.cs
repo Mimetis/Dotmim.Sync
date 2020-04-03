083389b0af8a60bdd8b46a44d04c2dcb0cd3de79
Sébastien Pertus <spertus@microsoft.com>
2020-04-03 22:36:55 +0200

    Update reference to Microsoft.Data.SqlClient

--- Tests/Dotmim.Sync.Tests/HelperDatabase.cs
+++ Tests/Dotmim.Sync.Tests/HelperDatabase.cs
@@ -4,7 +4,7 @@ using Microsoft.Data.Sqlite;
 using MySql.Data.MySqlClient;
 using System;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
