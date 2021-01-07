a2c84c7934220a1e1eecd71982e680766b397ed1
Sébastien Pertus <spertus@microsoft.com>
2021-01-07 21:20:09 +0100

    Adding HelloWorldAuth sample

---
+++ Samples/HelloWebAuthSync/HelloWebAuthSyncServer/Controllers/SyncController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dotmim.Sync.Web.Server;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWebSyncServer.Controllers
+{
+    //[Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SyncController : ControllerBase
+    {
+        // The WebServerManager instance is useful to manage all the Web server orchestrators register in the Startup.cs
+        private WebServerManager webServerManager;
+
+        // Injected thanks to Dependency Injection
+        public SyncController(WebServerManager webServerManager) => this.webServerManager = webServerManager;
+
+        /// <summary>
+        /// This POST handler is mandatory to handle all the sync process
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task Post()
+        {
+            // If you are using the [Authorize] attribute you don't need to check
+            // the User.Identity.IsAuthenticated value
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                await webServerManager.HandleRequestAsync(this.HttpContext);
+            }
+            else
+            {
+                this.HttpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            }
+        }
+
+        /// <summary>
+        /// This GET handler is optional. It allows you to see the configuration hosted on the server
+        /// The configuration is shown only if Environmenent == Development
+        /// </summary>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task Get() => await webServerManager.HandleRequestAsync(this.HttpContext);
+    }
+}
