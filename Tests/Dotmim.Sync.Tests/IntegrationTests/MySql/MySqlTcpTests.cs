009ab6255eef725487c7f21b724acff314f532a1
Sébastien Pertus <spertus@microsoft.com>
2021-02-08 10:42:24 +0100

    fix tests

--- Tests/Dotmim.Sync.Tests/IntegrationTests/MySql/MySqlTcpTests.cs
+++ Tests/Dotmim.Sync.Tests/IntegrationTests/MySql/MySqlTcpTests.cs
@@ -29,7 +29,7 @@ namespace Dotmim.Sync.Tests.IntegrationTests
         };
 
         public override List<ProviderType> ClientsType => new List<ProviderType>
-            { ProviderType.MySql};
+            { ProviderType.MySql,  ProviderType.MariaDB,  ProviderType.Sqlite,  ProviderType.Sql};
 
         public override ProviderType ServerType =>
             ProviderType.MySql;
