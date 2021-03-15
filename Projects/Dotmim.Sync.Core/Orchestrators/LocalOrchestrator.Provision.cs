3090c36d014da7d7eb4c3ff6a89ac13758a5a1e9
Sébastien Pertus <spertus@microsoft.com>
2021-03-15 19:28:57 +0100

    Add Deprovision with Save Scope

--- Projects/Dotmim.Sync.Core/Orchestrators/LocalOrchestrator.Provision.cs
+++ Projects/Dotmim.Sync.Core/Orchestrators/LocalOrchestrator.Provision.cs
@@ -42,23 +42,75 @@ namespace Dotmim.Sync
                 if (provision.HasFlag(SyncProvision.ServerHistoryScope) || provision.HasFlag(SyncProvision.ServerScope))
                     throw new InvalidProvisionForLocalOrchestratorException();
 
-                // Get client scope if not supplied
+                // Get server scope if not supplied
                 if (clientScopeInfo == null)
-                    clientScopeInfo = await this.GetClientScopeAsync(connection, transaction, cancellationToken, progress).ConfigureAwait(false);
+                {
+                    var scopeBuilder = this.GetScopeBuilder(this.Options.ScopeInfoTableName);
 
-                schema = await InternalProvisionAsync(ctx, overwrite, schema, provision, connection, transaction, cancellationToken, progress).ConfigureAwait(false);
+                    var exists = await this.InternalExistsScopeInfoTableAsync(ctx, DbScopeType.Client, scopeBuilder, connection, transaction, cancellationToken, progress).ConfigureAwait(false);
 
-                // Affect good values
-                clientScopeInfo.Schema = schema;
-                clientScopeInfo.Setup = this.Setup;
+                    if (!exists)
+                        await this.InternalCreateScopeInfoTableAsync(ctx, DbScopeType.Client, scopeBuilder, connection, transaction, cancellationToken, progress).ConfigureAwait(false);
 
-                // Save scope
-                var scopeBuilder = this.GetScopeBuilder(this.Options.ScopeInfoTableName);
-                await this.InternalSaveScopeAsync(ctx, DbScopeType.Client, clientScopeInfo, scopeBuilder, connection, transaction, cancellationToken, progress).ConfigureAwait(false);
+                    clientScopeInfo = await this.InternalGetScopeAsync<ScopeInfo>(ctx, DbScopeType.Client, this.ScopeName, scopeBuilder, connection, transaction, cancellationToken, progress).ConfigureAwait(false);
+                }
+
+                schema = await InternalProvisionAsync(ctx, overwrite, schema, provision, clientScopeInfo, connection, transaction, cancellationToken, progress).ConfigureAwait(false);
 
                 return schema;
 
             }, connection, transaction, cancellationToken);
+
+
+        /// <summary>
+        /// Deprovision the orchestrator database based on the orchestrator Setup instance, provided on constructor, and the provision enumeration
+        /// </summary>
+        /// <param name="provision">Provision enumeration to determine which components to deprovision</param>
+        public virtual Task DeprovisionAsync(SyncProvision provision, ScopeInfo clientScopeInfo = null, DbConnection connection = default, DbTransaction transaction = default, CancellationToken cancellationToken = default, IProgress<ProgressArgs> progress = null)
+        {
+            // Create a temporary SyncSet for attaching to the schemaTable
+            var tmpSchema = new SyncSet();
+
+            // Add this table to schema
+            foreach (var table in this.Setup.Tables)
+                tmpSchema.Tables.Add(new SyncTable(table.TableName, table.SchemaName));
+
+            tmpSchema.EnsureSchema();
+
+            // copy filters from old setup
+            foreach (var filter in this.Setup.Filters)
+                tmpSchema.Filters.Add(filter);
+
+            return this.DeprovisionAsync(tmpSchema, provision, clientScopeInfo, connection, transaction, cancellationToken, progress);
+        }
+
+        /// <summary>
+        /// Deprovision the orchestrator database based on the schema argument, and the provision enumeration
+        /// </summary>
+        /// <param name="schema">Schema to be deprovisioned from the database managed by the orchestrator, through the provider.</param>
+        /// <param name="provision">Provision enumeration to determine which components to deprovision</param>
+        public virtual Task DeprovisionAsync(SyncSet schema, SyncProvision provision, ScopeInfo clientScopeInfo = null, DbConnection connection = default, DbTransaction transaction = default, CancellationToken cancellationToken = default, IProgress<ProgressArgs> progress = null)
+        => RunInTransactionAsync(SyncStage.Deprovisioning, async (ctx, connection, transaction) =>
+        {
+
+            // Get server scope if not supplied
+            if (clientScopeInfo == null)
+            {
+                var scopeBuilder = this.GetScopeBuilder(this.Options.ScopeInfoTableName);
+
+                var exists = await this.InternalExistsScopeInfoTableAsync(ctx, DbScopeType.Client, scopeBuilder, connection, transaction, cancellationToken, progress).ConfigureAwait(false);
+
+                if (!exists)
+                    await this.InternalCreateScopeInfoTableAsync(ctx, DbScopeType.Client, scopeBuilder, connection, transaction, cancellationToken, progress).ConfigureAwait(false);
+
+                clientScopeInfo = await this.InternalGetScopeAsync<ScopeInfo>(ctx, DbScopeType.Client, this.ScopeName, scopeBuilder, connection, transaction, cancellationToken, progress).ConfigureAwait(false);
+            }
+
+            var isDeprovisioned = await InternalDeprovisionAsync(ctx, schema, provision, clientScopeInfo, connection, transaction, cancellationToken, progress).ConfigureAwait(false);
+
+            return isDeprovisioned;
+
+        }, connection, transaction, cancellationToken);
 
     }
 }
