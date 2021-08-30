4eac50654ac29bc2a4a835017c94b9554efbad75
Sébastien Pertus <spertus@microsoft.com>
2021-08-30 16:16:13 +0200

    Debug because it's cool ....

--- Tests/Dotmim.Sync.Tests/IntegrationTests/SqlServer/SqlServerHttpFilterTests.cs
+++ Tests/Dotmim.Sync.Tests/IntegrationTests/SqlServer/SqlServerHttpFilterTests.cs
@@ -18,7 +18,7 @@ namespace Dotmim.Sync.Tests.IntegrationTests
     {
 
         public override List<ProviderType> ClientsType => new List<ProviderType>
-            {ProviderType.Sql, ProviderType.MySql,  ProviderType.Sqlite};
+            {  ProviderType.Sql, ProviderType.Sqlite};
         public SqlServerHttpFilterTests(HelperProvider fixture, ITestOutputHelper output) : base(fixture, output)
         {
         }
