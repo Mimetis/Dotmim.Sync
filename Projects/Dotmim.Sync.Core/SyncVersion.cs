9fc91ff20349ecb7f90dd4ff7d13a6a0e07416e1
Sébastien Pertus <spertus@microsoft.com>
2021-11-14 17:15:48 +0100

    Change sqlite version. Tag 0.9.1

--- Projects/Dotmim.Sync.Core/SyncVersion.cs
+++ Projects/Dotmim.Sync.Core/SyncVersion.cs
@@ -6,7 +6,7 @@ namespace Dotmim.Sync
 {
     public static class SyncVersion
     {
-        public static Version Current { get; } = new Version(0, 9, 0);
+        public static Version Current { get; } = new Version(0, 9, 1);
 
         public static Version EnsureVersion(string v) => v == "1" ? new Version(0, 5, 7) : new Version(v);
     }
