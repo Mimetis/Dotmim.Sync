a0b78e06ffe18f1fae998a8c213b5e80848850af
Sébastien Pertus <spertus@microsoft.com>
2021-12-15 13:29:12 +0100

    add win forms samples

---
+++ Samples/WinFormsCore/Program.cs
@@ -0,0 +1,15 @@
+namespace WinFormsCore
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Samples/WindowsFormsClassic/Form1.cs
@@ -0,0 +1,55 @@
+﻿using Dotmim.Sync;
+using Dotmim.Sync.Sqlite;
+using Dotmim.Sync.SqlServer;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsClassic
+{
+    public partial class Form1 : Form
+    {
+        private static string serverConnectionString = $"Data Source=(localdb)\\mssqllocaldb; Initial Catalog=AdventureWorks;Integrated Security=true;";
+        private static string clientConnectionString = $"Data Source=(localdb)\\mssqllocaldb; Initial Catalog=Client;Integrated Security=true;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // Database script used for this sample : https://github.com/Mimetis/Dotmim.Sync/blob/master/CreateAdventureWorks.sql 
+
+            // Create 2 Sql Sync providers
+            // First provider is using the Sql change tracking feature. Don't forget to enable it on your database until running this code !
+            // For instance, use this SQL statement on your server database : ALTER DATABASE AdventureWorks  SET CHANGE_TRACKING = ON  (CHANGE_RETENTION = 10 DAYS, AUTO_CLEANUP = ON)  
+            // Otherwise, if you don't want to use Change Tracking feature, just change 'SqlSyncChangeTrackingProvider' to 'SqlSyncProvider'
+            var serverProvider = new SqlSyncProvider(serverConnectionString);
+
+            // Second provider is using plain old Sql Server provider, relying on triggers and tracking tables to create the sync environment
+            var clientProvider = new SqliteSyncProvider("adv.db");
+
+            // Tables involved in the sync process:
+            var tables = new string[] {"ProductCategory", "ProductModel", "Product",
+                        "Address", "Customer", "CustomerAddress", "SalesOrderHeader", "SalesOrderDetail" };
+
+            // Creating an agent that will handle all the process
+            var agent = new SyncAgent(clientProvider, serverProvider, tables);
+
+            // Launch the sync process
+            var s1 = await agent.SynchronizeAsync();
+
+            // Write results
+            MessageBox.Show(s1.ToString());
+
+        }
+    }
+}
