f5c11b8bd8b2708c4c0c591e2456c7301eb197bb
Sébastien Pertus <spertus@microsoft.com>
2022-12-28 19:27:23 +0100

    fix test to target postges instead of mysql

--- Tests/Dotmim.Sync.Tests/IntegrationTests/Postgres/PostgresTcpConflictsTests.cs
+++ Tests/Dotmim.Sync.Tests/IntegrationTests/Postgres/PostgresTcpConflictsTests.cs
@@ -19,7 +19,7 @@ namespace Dotmim.Sync.Tests.IntegrationTests
     public class PostgresTcpConflictsTests : TcpConflictsTests
     {
         public override List<ProviderType> ClientsType => new List<ProviderType>
-            { ProviderType.MySql,  ProviderType.Sql};
+            { ProviderType.Postgres,  ProviderType.Sql};
 
         public PostgresTcpConflictsTests(HelperProvider fixture, ITestOutputHelper output) : base(fixture, output)
         {
