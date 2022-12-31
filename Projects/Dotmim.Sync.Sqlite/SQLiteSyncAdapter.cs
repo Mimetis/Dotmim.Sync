554cf21a1a6560228a635c028c8e909f8bf8e702
Sébastien Pertus <spertus@microsoft.com>
2022-12-31 19:52:29 +0100

    fix

--- Projects/Dotmim.Sync.Sqlite/SQLiteSyncAdapter.cs
+++ Projects/Dotmim.Sync.Sqlite/SQLiteSyncAdapter.cs
@@ -79,13 +79,6 @@ namespace Dotmim.Sync.Sqlite
                 case DbCommandType.UpdateMetadata:
                     this.SetUpdateMetadataParameters(command);
                     break;
-                case DbCommandType.PreDeleteRow:
-                case DbCommandType.PreDeleteRows:
-                case DbCommandType.PreInsertRow:
-                case DbCommandType.PreInsertRows:
-                case DbCommandType.PreUpdateRow:
-                case DbCommandType.PreUpdateRows:
-                    return (default, false);
                 default:
                     break;
             }
