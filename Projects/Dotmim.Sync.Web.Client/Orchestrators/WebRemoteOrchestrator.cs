e518ca53ee1210a1f374a7dbd87d24de55ed258d
Sébastien Pertus <spertus@microsoft.com>
2024-06-12 10:38:32 +0200

    fix #1186

--- Projects/Dotmim.Sync.Web.Client/Orchestrators/WebRemoteOrchestrator.cs
+++ Projects/Dotmim.Sync.Web.Client/Orchestrators/WebRemoteOrchestrator.cs
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 using Dotmim.Sync.Batch;
 using Dotmim.Sync.Enumerations;
 using Dotmim.Sync.Serialization;
-#if NET6_0 || NET8_0 
+#if NET8_0 
 using Microsoft.Net.Http.Headers;
 #endif
 using Newtonsoft.Json;
