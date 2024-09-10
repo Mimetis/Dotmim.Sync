73e9e5054a94498845f1727d6c5268e1916833bf
Edward Miller <symbiogenesis@outlook.com>
2024-09-10 17:05:43 -0500

    cache the current SyncVersion.

--- Projects/Dotmim.Sync.Core/SyncVersion.cs
+++ Projects/Dotmim.Sync.Core/SyncVersion.cs
@@ -8,20 +8,16 @@ namespace Dotmim.Sync
     /// </summary>
     public static class SyncVersion
     {
+        private static readonly Lazy<Version> lazyVersion = new Lazy<Version>(() =>
+        {
+            return new Version(typeof(CoreProvider).GetTypeInfo()
+                .Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version);
+        });
 
         /// <summary>
         /// Gets the current version of the library.
         /// </summary>
-        public static Version Current
-        {
-            get
-            {
-                var assemblyVersion = new Version(typeof(CoreProvider).GetTypeInfo()
-                    .Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version);
-
-                return assemblyVersion;
-            }
-        }
+        public static Version Current => lazyVersion.Value;
 
         /// <summary>
         /// Ensure the version is correct.
